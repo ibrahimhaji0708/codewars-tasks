33225b53878da07218b9d906de011cb99f3fd8f6
ibrahimhaji0708 <98814268+ibrahimhaji0708@users.noreply.github.com>
2023-09-11 13:56:44 +0530

    init

--- test/lib/reverse_list_order_test.dart
+++ test/lib/reverse_list_order_test.dart
@@ -1,11 +1,28 @@
+import 'package:test/test.dart';
+import 'package:codewars_';
+
 List<int> reverseList(List<int> list) {
-  // Use the 'reversed' method to reverse the list.
   return list.reversed.toList();
 }
 
 void main() {
-  // Example usage:
-  List<int> originalList = [1, 2, 3, 4];
-  List<int> reversed = reverseList(originalList);
-  print(reversed); // Output: [4, 3, 2, 1]
+  group("reverseList function tests", () {
+    test("Test with a list of integers", () {
+      List<int> originalList = [1, 2, 3, 4];
+      List<int> reversed = reverseList(originalList);
+      expect(reversed, equals([4, 3, 2, 1]));
+    });
+
+    test("Test with an empty list", () {
+      List<int> originalList = [];
+      List<int> reversed = reverseList(originalList);
+      expect(reversed, equals([]));
+    });
+
+    test("Test with a single-element list", () {
+      List<int> originalList = [42];
+      List<int> reversed = reverseList(originalList);
+      expect(reversed, equals([42]));
+    });
+  });
 }
